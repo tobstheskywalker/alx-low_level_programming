04c15caba8c94d8138577210889f363dcf1e873d
Tobi <tobiloba2569@gmail.com>
2022-09-12 11:24:19 -0700

    print f

---
+++ 0x00-hello-world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+/**
+* main - print the string in put function 
+* 
+* Description: using the main function 
+* this program prints "Programming is like a multilingual puzzle
+* Return: 0
+*/ 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+} 
+^C
