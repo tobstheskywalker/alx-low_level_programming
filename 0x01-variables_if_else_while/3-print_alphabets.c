6d5522c29fbe13d8fd81e8df4254f6b42d04c3eb
Tobi <tobiloba2569@gmail.com>
2022-09-12 16:48:24 -0700

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - print if the number is positive, zero or negative
- * 
- * Description: using the main function 
- * this program prints "Programming is positive, zero or negative 
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etcâ€¦) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
  * Return: 0
  */
-int main(void) 
+int main(void)
 {
-char ch;
-for (ch = Ã'a' ; ch <= 'z' ; ch++)
-{ 
-	putchar(ch); 
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
 }
-for (ch = Ã'A' ; ch <= 'Z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n'); 
-return (0); 
-} 
-^C
