7b2e7b9c1bb56bcf247efb01ac47a2d0c53a6e7a
Tobi <tobiloba2569@gmail.com>
2022-09-29 19:48:43 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse.
+ * _puts_recursion - prints a string, followed by a new line.
  * @s: the string
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
+		_putchar('\n');
 		return;
+	}
 
-	_print_rev_recursion(s + 1);
 	_putchar(*s);
+	_puts_recursion(++s);
 }
